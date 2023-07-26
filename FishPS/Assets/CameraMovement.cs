02de267254c78a8d1dd3fa24621bf21c9fd74d17
Hpsbsp55 <vaughan.walkosak@gmail.com>
2023-07-26 16:45:59 -0400

    Created mouse movement script and created a parent gameobject to Main Camera

---
+++ FishPS/Assets/CameraMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    [SerializeField] float lookSpeed;
+    [SerializeField] float VRotation;
+    //public Camera Main;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //confines cursor to window
+        Cursor.visible = false; //makes cursor invisible
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 mPos = Input.mousePosition;
+        float lookX = Input.GetAxis("Mouse X") * lookSpeed;
+        float lookY = Input.GetAxis("Mouse Y") * lookSpeed;
+        VRotation -= lookY;
+        VRotation = Mathf.Clamp(VRotation, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * VRotation;
+        player.transform.Rotate(Vector3.up * lookX);
+        //transform.LookAt(Input.mousePosition - Main.ScreenToWorldPoint(transform.position));
+    }
+}
